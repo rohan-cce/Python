6e082c299b65bc6b2775b7ac8e9eba406b518230
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-10-17 21:07:28 +0530

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,8 @@
+import base64
+
+with open('logo.png', 'rb') as binary_file:
+    binary_file_data = binary_file.read()
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    base64_message = base64_encoded_data.decode('utf-8')
+
+    print(base64_message)
